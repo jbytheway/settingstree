9f68c3726339fe6a31a7cf32919e5f16d9793e5e
John Bytheway <jbytheway@gmail.com>
2009-09-29 22:02:03 +0100

    Switch to Boost.Test and test getRequest
    Signed-off-by: John Bytheway <jbytheway@gmail.com>

---
+++ test/first.cpp
@@ -0,0 +1,76 @@
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <memory>
+
+#include <boost/assign/list_of.hpp>
+
+#include <settingstree/tree.hpp>
+#include <settingstree/make.hpp>
+
+namespace st = settingstree;
+
+class test_callback :
+  public st::leaf_callback<bool>,
+  public st::leaf_callback<int>,
+  public st::leaf_callback<std::string>
+{
+  virtual std::string settingAlteringCallback(st::leaf*, bool) {
+    std::cout << "altering" << std::endl;
+    return "";
+  }
+  virtual std::string settingAlteringCallback(st::leaf*, int) {
+    std::cout << "altering" << std::endl;
+    return "";
+  }
+  virtual std::string settingAlteringCallback(st::leaf*, std::string) {
+    std::cout << "altering" << std::endl;
+    return "";
+  }
+  virtual void settingAlteredCallback(st::leaf*) {
+    std::cout << "altered" << std::endl;
+  }
+};
+
+class test_branch_callback : public st::branch_callback {
+  virtual void childrenAlteredCallback(st::branch*) {
+    std::cout << "children altered" << std::endl;
+  }
+};
+
+class server_user : public st::settings_user {
+  public:
+    server_user() : st::settings_user("server") {}
+};
+
+BOOST_AUTO_TEST_CASE(first)
+{
+  test_callback c;
+  test_branch_callback bc;
+
+  st::tree::ptr tree =
+    st::make("", bc,
+      st::make("var_bool", static_cast<st::leaf_callback<bool>&>(c), true),
+      st::make("subtree", bc,
+        st::make("var_int", static_cast<st::leaf_callback<int>&>(c), 0),
+        st::make("var_string", c, "flibble")
+      )
+    ).tree_ptr();
+
+  server_user su;
+
+  st::node* bool_node = tree->getNode("var_bool");
+  BOOST_CHECK(bool_node);
+
+  std::string result;
+  std::set<std::string> value;
+  st::node const* node;
+
+  boost::tie(result, value, node) = tree->getRequest("var_bool", &su);
+
+  BOOST_CHECK_EQUAL(result, "");
+  BOOST_CHECK(value == boost::assign::list_of("true"));
+  BOOST_CHECK_EQUAL(node, bool_node);
+}
+
